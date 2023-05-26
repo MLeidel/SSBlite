0873438120f2f2530c8e1db0a73fb5ded51275a5
MLeidel <mike.leidel@gmail.com>
2023-05-26 13:17:58 -0500

    update

---
+++ ssb.py
@@ -0,0 +1,134 @@
+# ssb.py
+# Lite_Site Specific Browser
+#   with minimal menu based navigation.
+
+# CAUTION: MANY NORMAL BROWSER FEATURES MISSING !!!
+# (LIKE: tabs, cookies, sessions, page-search, right-click menu, downloads, and more)
+
+# based on pywebview 4.0.2 & Python 3..
+# Use:
+# $ python3 ssb.py {title | "no"} {app URL}
+#   "no" means the URL will be in the window caption
+#   otherwise title will be in the window caption
+
+import sys, os
+import webview
+import webview.menu as wm
+
+# initialize window metrics
+top = 50
+left = 50
+win_width = 600
+win_height = 600
+lst = []
+
+if len(sys.argv) < 3:
+    print("Missing args: {title | \"no\"} {URL}")
+    sys.exit()
+
+URL = sys.argv[2]
+TITLE = sys.argv[1]
+
+# change working directory to path of program
+p = os.path.realpath(__file__)
+os.chdir(os.path.dirname(p))
+
+# read windows metrics
+with open('ssb_winfo', 'r') as f:
+    line = f.readline().strip()
+    lst = line.split(',')
+    lst = [int(num) for num in lst]
+    top = lst[0]
+    left = lst[1]
+    win_width = lst[2]
+    win_height = lst[3]
+
+# ---------------------------------------------------------------
+
+def on_closed():
+    ''' write window metrics '''
+    global top, left, win_width, win_height
+    with open("ssb_winfo", "w") as f:
+        f.write(",".join( (str(top),
+                           str(left),
+                           str(win_width),
+                           str(win_height)
+                          )))
+
+def on_loaded():
+    ''' Put the page URL in the title for every new page '''
+    active_window = webview.active_window()
+    if TITLE != "no":
+        return
+    else:
+        if active_window:
+            active_window.set_title(active_window.get_current_url())
+    # active_window.get_cookies()
+
+def home_ssd():
+    win.load_url(URL)
+
+def back_ssd():
+    # win.evaluate_js("history.back()")
+    active_window = webview.active_window()
+    if active_window:
+        active_window.evaluate_js("history.back()")
+
+def forward_ssd():
+    win.evaluate_js("history.forward()")
+
+def reload_ssd():
+     win.evaluate_js("location.reload()")
+
+def exit_ssd():
+    win.destroy()
+
+def search_ssd():
+    ''' new search text or URL '''
+    js = '''
+    let txt = prompt("Enter URL or Search Prompt", "")
+    if (txt != null) {
+        if (txt.startsWith("http")) {
+            location = txt;
+        } else {
+            location = "https://search.google.com/search?q=" + txt;
+        }
+    }
+    '''
+    active_window = webview.active_window()
+    if active_window:
+        active_window.evaluate_js(js)
+
+def on_resized():
+    global top, left, win_width, win_height
+    left, top = win.x, win.y
+    win_width, win_height = win.width, win.height
+
+
+# create browser window with title and url
+win = webview.create_window(TITLE,
+                            url=URL,
+                            width=win_width,
+                            height=win_height,
+                            x=left,
+                            y=top)
+win.events.resized += on_resized
+win.events.loaded += on_loaded
+win.events.closed += on_closed
+
+
+menu_items = [
+    wm.Menu(
+        '«=»',
+        [
+            wm.MenuAction('Back', back_ssd),
+            wm.MenuAction('Forward', forward_ssd),
+            wm.MenuAction('Search or URL', search_ssd),
+            wm.MenuAction('Home', home_ssd),
+            wm.MenuAction('Reload', reload_ssd),
+            wm.MenuAction('Exit', exit_ssd),
+        ],
+    )
+]
+
+webview.start(menu=menu_items)
